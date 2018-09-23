a0c35321f0d529b6e458a5758283e983c226f7ff
rektide de la faye <rektide@voodoowarez.com>
2018-09-23 18:26:41 -0400

    Caching per object.
    Uncommitted old work, and I'm skeptical throwing a weakmap cache in
    by default is good. But here it is for now.

--- get-property-descriptor.js
+++ get-property-descriptor.js
@@ -1,11 +1,26 @@
 "use strict"
 
+const _cache= new WeakMap()
+
 export function getPropertyDescriptor(o, prop){
-	var proto = o,
+	let
+	  had= _cache.get( o),
+	  proto= o,
 	  descriptor
+	if( had){
+		descriptor= had[ prop]
+		if( descriptor!== undefined){
+			//return {...descriptor}
+			return descriptor
+		}
+	}else{
+		had= {}
+		_cache.set( o, prop, had)
+	}
 	while (proto && !(descriptor = Object.getOwnPropertyDescriptor(proto, prop))) {
 		proto = Object.getPrototypeOf(proto)
 	}
+	had[ prop]= descriptor
 	return descriptor
 }
 export default getPropertyDescriptor
