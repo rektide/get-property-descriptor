b5abee39cbb6f47c85cad1ed1cee175a23a55d19
rektide de la faye <rektide@voodoowarez.com>
2019-01-20 19:39:44 -0500

    Remove caching stuff from get-property-descriptor.

--- get-property-descriptor.js
+++ get-property-descriptor.js
@@ -1,26 +1,12 @@
 "use strict"
 
-const _cache= new WeakMap()
-
 export function getPropertyDescriptor(o, prop){
 	let
-	  had= _cache.get( o),
 	  proto= o,
 	  descriptor
-	if( had){
-		descriptor= had[ prop]
-		if( descriptor!== undefined){
-			//return {...descriptor}
-			return descriptor
-		}
-	}else{
-		had= {}
-		_cache.set( o, prop, had)
-	}
 	while (proto && !(descriptor = Object.getOwnPropertyDescriptor(proto, prop))) {
 		proto = Object.getPrototypeOf(proto)
 	}
-	had[ prop]= descriptor
 	return descriptor
 }
 export default getPropertyDescriptor
